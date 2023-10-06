c470d792fc6355bef25b731b8f238010429fc669
Aryan <aryancr72000@gmail.com>
2023-10-06 21:17:04 +0530

    created api for employees and company

--- restapi/models.py
+++ restapi/models.py
@@ -11,5 +11,15 @@ class Company(models.Model):
     about=models.TextField()
     type=models.CharField(max_length=100,choices=(('IT','IT'),('Non IT','Non IT'),('Mobiles','Mobiles')))
     added=models.DateTimeField(auto_now=True)
-    active=models.BooleanField(default=True)    
+    active=models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name+" "+self.location 
+
+#Employee Model
+
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=50)
+    address=models.TextField()
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from restapi.models import Company,Employee
+from restapi.serializers import CompanySerializer,EmployeeSerializer
 # Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset=Company.objects.all()
+    serializer_class=CompanySerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializer 
