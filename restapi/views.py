b85d887cf44a720bb974b0d6e54ded4bf907835d
Aryan <aryancr72000@gmail.com>
2023-10-10 21:40:31 +0530

    created action for finding employees in company

--- restapi/views.py
+++ restapi/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from rest_framework import viewsets
 from restapi.models import Company,Employee
 from restapi.serializers import CompanySerializer,EmployeeSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 # Create your views here.
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset=Company.objects.all()
     serializer_class=CompanySerializer
+
+    @action(detail=True,methods=['get'])
+    def employees(self,request,pk=None):
+        company=self.get_object()
+        employees=Employee.objects.filter(company=company)
+        serializer=EmployeeSerializer(employees,many=True,context={'request': request})
+        return Response(serializer.data)
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset=Employee.objects.all()
